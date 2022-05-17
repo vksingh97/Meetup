99d5e2b6dadaa64b8298603d971af9b9e420411d
Vivek <viveksingh.singh50@gmail.com>
2022-05-17 14:26:17 +0530

    added user login endpoint

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -27,3 +27,28 @@ exports.register = async (req, res) => {
     });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect password" });
+    }
+
+    const token = await user.generateToken();
+
+    res.status(200).cookie("token", token).json({ success: true, user, token });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { register } = require("../controller/user");
+const { register, login } = require("../controller/user");
 
 const router = express.Router();
 
 router.route("/register").post(register);
+router.route("/login").post(login);
 
 module.exports = router;
