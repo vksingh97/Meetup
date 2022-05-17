4deffd487f51cd4cf4f4cf181d2679850e956cd3
Vivek <viveksingh.singh50@gmail.com>
2022-05-17 15:03:50 +0530

    like and dislike post route created

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createPost } = require("../controller/post");
+const { createPost, likeAndUnlikePost } = require("../controller/post");
 const { isAuthenticated } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/post/upload").post(isAuthenticated, createPost);
+router.route("/post/:id").get(isAuthenticated, likeAndUnlikePost);
 
 module.exports = router;
