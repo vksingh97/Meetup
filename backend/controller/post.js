3b4e502c04dd70e8d1631d6dbfc904f2bb359060
Vivek <viveksingh.singh50@gmail.com>
2022-05-17 23:09:10 +0530

    created delete route

--- backend/controller/post.js
+++ backend/controller/post.js
@@ -29,6 +29,38 @@ exports.createPost = async (req, res) => {
   }
 };
 
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    if (post.owner.toString() != req.user._id.toString()) {
+      return res.status(404).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+    await post.remove();
+
+    const user = await User.findById(req.user._id);
+    const index = user.posts.indexOf(req.params.id);
+    user.posts.splice(index, 1);
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Post deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
 exports.likeAndUnlikePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const { createPost, likeAndUnlikePost } = require("../controller/post");
+const {
+  createPost,
+  likeAndUnlikePost,
+  deletePost,
+} = require("../controller/post");
 const { isAuthenticated } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/post/upload").post(isAuthenticated, createPost);
-router.route("/post/:id").get(isAuthenticated, likeAndUnlikePost);
+router
+  .route("/post/:id")
+  .get(isAuthenticated, likeAndUnlikePost)
+  .delete(isAuthenticated, deletePost);
 
 module.exports = router;
