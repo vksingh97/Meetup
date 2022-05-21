0b19c39bf31829f6344d8beccfb7bbaceff16dd5
Vivek <viveksingh.singh50@gmail.com>
2022-05-21 11:51:07 +0530

    logout route added

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -68,6 +68,17 @@ exports.login = async (req, res) => {
   }
 };
 
+exports.logout = async (req, res) => {
+  try {
+    res
+      .status(200)
+      .cookie("token", null, { expires: new Date(Date.now()), httpOnly: true })
+      .json({ success: true, message: "Logged Out" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
 exports.followUser = async (req, res) => {
   try {
     const userToFollow = await User.findById(req.params.id);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, followUser } = require("../controller/user");
+const { register, login, followUser, logout } = require("../controller/user");
 const { isAuthenticated } = require("../middleware/auth");
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/follow/:id").get(isAuthenticated, followUser);
+router.route("/logout").get(logout);
 
 module.exports = router;
